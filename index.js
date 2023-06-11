c8c27dc1ebefda655a3a8ef3c50ded4842125e1b
IrisLegend <hiroshitanaka0629@gmail.com>
2023-06-11 06:27:19 -0700

    update: reply

--- index.js
+++ index.js
@@ -4,33 +4,59 @@ dotenv.config();
 
 // Create a Client instance with our bot token.
 const bot = new eris.Client(process.env.BOT_TOKEN);
+const PREFIX = 'iris#';
+
+const commandHandlerForCommandName = {};
+
+commandHandlerForCommandName.addPayment = (msg, args) => {
+    const mention = args[0]
+    const amount = parseFloat(args[1]);
+
+    // TODO: Handle invalid command arguments, such as:
+    // 1. No mention or invalid mention
+    // 2. No amount or invalid amount
+    return msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`);
+}
 
 // When the bot is connected and ready, log to console.
-bot.on('ready', () => {
-    console.log(bot);
-    console.log('Connected and ready.');
-});
+bot.on('ready', () =>  console.log('Connected and ready.'));
 
 // Every time a message is sent anywhere the bot is present,
 // this event will fire and we will check if the bot was mentioned.
 // If it was, the bot will attempt to respond with "Present".
 bot.on('messageCreate', async (msg) => {
-    console.log(msg);
-    const botWasMentioned = msg.mentions.find(
-        mentionedUser => mentionedUser.id === bot.user.id,
-    );
+    const content = msg.content;
+    
+    //Ignore any messages sent as direct messages.
+    // The bot will only accept commands issued in a guild.
+    if(!msg.channel.guild) {
+        return;
+    }
 
-    if (botWasMentioned) {
-        try {
-            await msg.channel.createMessage('Present');
-        } catch (err) {
-            // There are various reasons why sending a message may fail.
-            // The API might time out or choke and return a 5xx status,
-            // or the bot may not have permission to send the
-            // message (403 status).
-            console.warn('Failed to respond to mention.');
-            console.warn(err);
-        }
+    //Ignore any message that doesn't start with correct prefix.
+    if(!content.startsWith(PREFIX)) {
+        return;
+    }
+
+    // Extract the parts of the command and command name.
+    const parts = content.split(' ').map(s => s.trim()).filter(s => s);
+
+    const commandName = parts[0].substr(PREFIX.length);
+
+    //Get the appropriate handler for the command. if there is one.
+    const commandHandler = commandHandlerForCommandName = [commandName];
+    if(!commandHandler) {
+        return;
+    }
+
+    // Seperate the command arguments from command prefix and command name.
+    const args = parts.slice(1);
+
+    try {
+        await commandHandler(msg, args);
+    } catch (err) {
+        msg.reply('Error handle command');
+        msg.reply(err);
     }
 });
 
